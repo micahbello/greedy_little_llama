f1e4f5df7e01e8a60fbc224bb96ccee79ed0413c
Micah <micahbello@gmail.com>
2018-04-01 14:44:06 -0400

    Work on different types of ball objects

--- jumper.js
+++ jumper.js
@@ -2,6 +2,7 @@ import Game from './lib/game';
 import Jumper from './lib/jumping_person';
 import Ledge from './lib/ledge';
 import Ball from './lib/ball';
+import Prize from './lib/prize';
 
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -14,19 +15,10 @@ document.addEventListener("DOMContentLoaded", function() {
   let jumperMan = new Jumper("red", 25, 25, 0, 275)
   //the Balls
 
-  let balls = [
-    new Ball("blue", 100, 300, 5),
-    new Ball("black", 10, 20, 5),
-    new Ball("red", 10, 300, 5),
-    new Ball("green", 200, 300, 5)];
+  let balls = [new Ball("black", 200, 300)];
 
   //the ledges
   let ledges = [];
-
-
-  let randomSpeed = (min, max) => {
-    return Math.random() * (max - min) + min;
-  }
 
   let randomXCoord = (min, max) => {
     return Math.random() * (max - min) + min;
@@ -36,24 +28,45 @@ document.addEventListener("DOMContentLoaded", function() {
     return Math.random() * (max - min) + min;
   }
 
+  let prize = new Prize(randomXCoord(10, 550), randomYCoord(0, 250))
+
+
+  let randomSpeed = (min, max) => {
+    return Math.random() * (max - min) + min;
+  }
+
+  randomXCoord = (min, max) => {
+    return Math.random() * (max - min) + min;
+  }
+
+  randomYCoord = (min, max) => {
+    return Math.random() * (max - min) + min;
+  }
+
 
   for (let i = 0; i < 10; i++) {
-     ledges.push(new Ledge("purple", -(randomXCoord(0, 600)), randomYCoord(50, 250),
+     ledges.push(new Ledge("blue", -(randomXCoord(0, 600)), randomYCoord(50, 250),
     randomSpeed(.1, .3)));
   }
 
-  // for (let i = 0; i < 1; i++) { // only for testing
-  //    ledges.push(new Ledge("purple", 50, 270,
-  //   randomSpeed(0, 0)));
-  // }
-
   //the gameloop
-  let game = new Game(jumperMan, balls, ledges, ctx);
+  let game = new Game(jumperMan, balls, ledges, prize, ctx);
+
 
   window.addEventListener("keydown", (e) => jumperMan.keyDownHandler(e));
   window.addEventListener("keyup", (e) => jumperMan.keyUpHandler(e));
 
-  setInterval(() => game.loop(), 1);
 
+
+  setInterval(() => game.loop(), 1);
+  setInterval(() => game.prize.prizeFlash(), 150)
+
+  let specialBalls = ["red", "green", "yellow", "brown"]
+
+  // setInterval(() => game.newBall("red"), 1000);
+
+
+  setInterval(() => game.newBall(specialBalls[Math.floor(Math.random() * 3)]), 100000);
+  // setInterval(() => game.checkBallStatus(), 1000)
 
 });

c4f814e10ffd09ae16ae57114008d333b3e6aa7c
Micah <micahbello@gmail.com>
2018-04-03 13:50:02 -0400

    Work on sprites

--- jumper.js
+++ jumper.js
@@ -12,10 +12,10 @@ document.addEventListener("DOMContentLoaded", function() {
   canvasEl.width = 600;
   canvasEl.height = 300;
   //the jumper
-  let jumperMan = new Jumper("red", 25, 15, 0, 275)
+  let jumperMan = new Jumper("red", 30, 25, 0, 275)
   //the Balls
 
-  let balls = [new Ball("black", 598, 3)];
+  let balls = [new Ball("black", 150, 150)];
 
   //the ledges
   let ledges = [];
@@ -30,24 +30,24 @@ document.addEventListener("DOMContentLoaded", function() {
 
   let prize = new Prize(randomXCoord(10, 550), randomYCoord(0, 250))
 
-
-  let randomSpeed = (min, max) => {
-    return Math.random() * (max - min) + min;
-  }
-
-  randomXCoord = (min, max) => {
-    return Math.random() * (max - min) + min;
-  }
-
-  randomYCoord = (min, max) => {
-    return Math.random() * (max - min) + min;
-  }
-
-
-  for (let i = 0; i < 10; i++) {
-     ledges.push(new Ledge("blue", -(randomXCoord(0, 600)), randomYCoord(50, 250),
-    randomSpeed(.1, .3)));
-  }
+  //
+  // let randomSpeed = (min, max) => {
+  //   return Math.random() * (max - min) + min;
+  // }
+  //
+  // randomXCoord = (min, max) => {
+  //   return Math.random() * (max - min) + min;
+  // }
+  //
+  // randomYCoord = (min, max) => {
+  //   return Math.random() * (max - min) + min;
+  // }
+  //
+  //
+  // // for (let i = 0; i < 10; i++) {
+  // //    ledges.push(new Ledge("blue", -(randomXCoord(0, 600)), randomYCoord(50, 250),
+  // //   randomSpeed(.1, .3)));
+  // // }
 
   //the gameloop
   let game = new Game(jumperMan, balls, ledges, prize, ctx);
@@ -58,13 +58,13 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
-  setInterval(() => game.loop(), 1);
+  setInterval(() => game.loop(), 2);
   setInterval(() => game.prize.prizeFlash(), 150)
 
   let specialBalls = ["red", "green", "yellow", "brown"]
 
-
   setInterval(() => game.newBall(specialBalls[Math.floor(Math.random() * 4)]), 15000);
-  setInterval(() => game.checkBallStatus(), 1000)
+  setInterval(() => game.checkBallStatus(), 1000);
+  setInterval(() => game.newLegdes(), 700);
 
 });

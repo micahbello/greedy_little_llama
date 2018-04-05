b314c0c86ec4486afde7d80e882832a2f2131c32
Micah <micahbello@gmail.com>
2018-04-05 14:50:42 -0400

    Refactor code and work on fixing ball objects get stuck bug

--- jumper.js
+++ jumper.js
@@ -11,64 +11,48 @@ document.addEventListener("DOMContentLoaded", function() {
 
   canvasEl.width = 600;
   canvasEl.height = 300;
-  //the jumper
+
   let jumperMan = new Jumper("red", 30, 25, 0, 275)
-  //the Balls
 
-  let balls = [new Ball("black", 150, 150)];
 
-  //the ledges
+  let balls = [
+  new Ball("black", 150, 150),
+  // new Ball("yellow", 10, 150), // TESTS
+  // // new Ball("pink", 100, 150), //
+  // new Ball("green", 50, 150), //
+  // new Ball("yellow", 10, 200), //
+  // // new Ball("pink", 100, 10), //
+  // new Ball("green", 50, 70) //
+  ];
+
   let ledges = [];
 
-  let randomXCoord = (min, max) => {
+
+  let randomXCoord = (min, max) => {                //used for placing the prize
     return Math.random() * (max - min) + min;
   }
 
-  let randomYCoord = (min, max) => {
+  let randomYCoord = (min, max) => {                //used for placing the prize
     return Math.random() * (max - min) + min;
   }
 
   let prize = new Prize(randomXCoord(10, 550), randomYCoord(0, 250))
 
-  //
-  // let randomSpeed = (min, max) => {
-  //   return Math.random() * (max - min) + min;
-  // }
-  //
-  // randomXCoord = (min, max) => {
-  //   return Math.random() * (max - min) + min;
-  // }
-  //
-  // randomYCoord = (min, max) => {
-  //   return Math.random() * (max - min) + min;
-  // }
-  //
-  //
-  // // for (let i = 0; i < 10; i++) {
-  // //    ledges.push(new Ledge("blue", -(randomXCoord(0, 600)), randomYCoord(50, 250),
-  // //   randomSpeed(.1, .3)));
-  // // }
 
-  //the gameloop
   let game = new Game(jumperMan, balls, ledges, prize, ctx);
+  let specialBalls = ["red", "green", "yellow", "brown"]
 
 
   window.addEventListener("keydown", (e) => game.keyDownHandler(e));
   window.addEventListener("keydown", (e) => jumperMan.keyDownHandler(e));
-
   window.addEventListener("keyup", (e) => jumperMan.keyUpHandler(e));
-  // window.addEventListener("keyup", (e) => game.keyUpHandler(e));
-
-
 
   setInterval(() => game.loop(), 2);
-
-  // setInterval(() => game.prize.prizeFlash(), 150)
-
-  let specialBalls = ["red", "green", "yellow", "brown"]
-
+  // setInterval(() => game.prize.prizeFlash(), 150)   //no longer needed since coin is spinning
   setInterval(() => game.newBall(specialBalls[Math.floor(Math.random() * 4)]), 15000);
-  setInterval(() => game.checkBallStatus(), 1000);
-  setInterval(() => game.newLegdes(), 700);
+  setInterval(() => game.checkJumperStatus(), 1000);
+  setInterval(() => game.newLegdes(), 700);     // commented out for testing
+
+  setInterval(() => game.detectLongCollision(), 350)
 
 });
